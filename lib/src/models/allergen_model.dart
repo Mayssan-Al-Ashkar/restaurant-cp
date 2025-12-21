f203fc228a5c19117c31628774987a8d87117132
Mayssan <mayssanalashkar@gmail.com>
2025-12-21 23:39:35 +0200

    add classes (models)

---
+++ lib/src/models/allergen_model.dart
@@ -0,0 +1,18 @@
+
+class AllergenModel {
+  AllergenModel({required this.id, required this.name});
+  int id;
+  String name;
+
+  factory AllergenModel.fromJson(Map<String, dynamic> json) => AllergenModel(
+    id: json["id"] ,
+    name: json["name"],
+  );
+
+  Map<String, dynamic> toJson() => {
+    "id": id,
+    "name": name,
+  };
+
+}
+
